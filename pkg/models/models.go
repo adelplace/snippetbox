3a8918eecf20041bd1b9f82896931381acf3ce3b
Alexandre Delplace <alexandre.delplace@smile.fr>
2019-04-06 14:07:04 +0200

    Fix id rendering

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -3,12 +3,14 @@ package models
 import (
 	"errors"
 	"time"
+
+	"github.com/mongodb/mongo-go-driver/bson/primitive"
 )
 
 var ErrNoRecord = errors.New("models: no matching record found")
 
 type Snippet struct {
-	ID      string
+	ID      primitive.ObjectID `bson:"_id"`
 	Title   string
 	Content string
 	Created time.Time
